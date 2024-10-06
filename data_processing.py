85d10e660d49a1aade92ccc485a9192b74347a4d
gouri1256 <gourijadhav12@gmail.com>
2024-10-06 17:33:35 +0530

    Added muktiplication and Sum function'

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,19 @@
 print('adding all the data processing code related Credit EDA assignment')
+
+
+a=5
+b=8
+
+def sum(a,b):
+    c=a+b
+    return c
+
+print('Sum function')
+
+
+def mul(a,b):
+    d=a*b
+    return b
+
+print('multiplication function')
+
